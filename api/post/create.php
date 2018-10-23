8a9d49bd6c28d2eeae236e356d5bf2df5f7cb47b
Babashov Mahammad <babashov.info@gmail.com>
2018-10-23 14:55:12 +0400

    Check Data is null or not?

--- api/post/create.php
+++ api/post/create.php
@@ -18,10 +18,15 @@ $post = new Post($db);
 // Get raw posted data
 $data = json_decode(file_get_contents('php://input'));
 
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author  = $data->author;
-$post->category_id = $data->category_id;
+// Checking Data from input is null?
+if($data != NULL)
+{
+  $post->title = $data->title;
+  $post->body = $data->body;
+  $post->author  = $data->author;
+  $post->category_id = $data->category_id;
+
+
 
 if($post->create())
 {
@@ -32,4 +37,12 @@ if($post->create())
   echo json_encode(
     array('message'=>'Post Not Created')
   );
+}
+
+}else{
+
+  echo json_encode(
+    array('message'=>'Post Data is Null')
+  );
+
 }
